919ac804c004e52d9f19c008d3da89fbfdac4ff6
rrupeshh <rupesh.poudel07@gmail.com>
2018-07-09 13:35:08 +0545

    Updated test_model

--- test_model.py
+++ test_model.py
@@ -1,7 +1,7 @@
 from keras.models import load_model
 
 classifier = load_model('Trained_model.h5')
-
+classifier.evaluate()
 
 #Prediction of single image
 import numpy as np
@@ -18,13 +18,55 @@ result = classifier.predict(test_image)
 print('Predicted Sign is:')
 print('')
 if result[0][0] == 1:
-       print('P')
+       return 'A'
 elif result[0][1] == 1:
-       print('Y')
+       return 'B'
 elif result[0][2] == 1:
-       print('Cool')
+       return 'C'
 elif result[0][3] == 1:
-       print('Five')
-else:
-       print('Like')
+       return 'D'
+elif result[0][4] == 1:
+       return 'E'
+elif result[0][5] == 1:
+       return 'F'
+elif result[0][6] == 1:
+       return 'G'
+elif result[0][7] == 1:
+       return 'H'
+elif result[0][8] == 1:
+       return 'I'
+elif result[0][9] == 1:
+       return 'J'
+elif result[0][10] == 1:
+       return 'K'
+elif result[0][11] == 1:
+       return 'L'
+elif result[0][12] == 1:
+       return 'M'
+elif result[0][13] == 1:
+       return 'N'
+elif result[0][14] == 1:
+       return 'O'
+elif result[0][15] == 1:
+       return 'P'
+elif result[0][16] == 1:
+       return 'Q'
+elif result[0][17] == 1:
+       return 'R'
+elif result[0][18] == 1:
+       return 'S'
+elif result[0][19] == 1:
+       return 'T'
+elif result[0][20] == 1:
+       return 'U'
+elif result[0][21] == 1:
+       return 'V'
+elif result[0][22] == 1:
+       return 'W'
+elif result[0][23] == 1:
+       return 'X'
+elif result[0][24] == 1:
+       return 'Y'
+elif result[0][25] == 1:
+       return 'Z'
 
